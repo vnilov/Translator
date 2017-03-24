eec1473f56ea85f2ccb41b252d5083ed56c0eb90
Нилов Вадим Валерьевич <vnilov@ctcmedia.ru>
2017-03-24 18:06:41 +0300

    activity fixed

--- app/src/main/java/com/example/vnilov/translator/MainActivity.java
+++ app/src/main/java/com/example/vnilov/translator/MainActivity.java
@@ -38,10 +38,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.title_translate);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_history);
 
                     try {
                         File file = new File(getCacheDir(), "getLangs");
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_favorites);
                     return true;
             }
             return false;
